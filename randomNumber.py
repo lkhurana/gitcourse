4cc95adcb259902e4521d9c0b1f2c030843d73fc
Lakshay Khurana <lkhurana@dpz.eu>
2020-06-15 15:40:09 +0000

    correct files, one for number, one for colour

--- randomNumber.py
+++ randomNumber.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# team: Stefani Diaz, Lakshay Khurana, Panda
-# expert of exercise block 1: Stefani Diaz
+# team: tesuser 1, testuser 2, testuser 3
 
 import random
 import time
@@ -14,34 +13,12 @@ def get_random_number(start, end):
 
 
 def write_log_file(outputfilename, data):
-    f = open(outputfilename+".log", "a")
-    f.write("Our randomly generated color is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
+    f = open(outputfilename + ".log", "a")
+    f.write("My randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
     f.close()
-
-
-def get_color_by_dice(spots):
-    colors = ["blue", "green", 'red', 'yellow', 'purple', 'orange']
-    return colors[spots-1]
-
-
-def get_color_by_dice_naive(spots):
-    if spots == 1:
-        color = 'blue'
-    elif spots == 2:
-        color = 'green'
-    elif spots == 3:
-        color = 'red'
-    elif spots == 4:
-        color = 'yellow'
-    elif spots == 5:
-        color = 'purple'
-    else: # spots == 6:
-        color = 'orange'
-    return color
 
 
 if __name__ == "__main__":
     outputfilename = "randomNumber"
-    roll = get_random_number(1, 6)
-    color = get_color_by_dice(roll)
-    write_log_file(outputfilename, color)
+    roll = get_random_number(1, 100)
+    write_log_file(outputfilename, roll)
